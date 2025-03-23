2594d8dd548ea91fa0fd04091f26e68ee84c1026
Mastracci <38020285+MastracciN@users.noreply.github.com>
2025-03-23 12:38:07 -0400

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-
   return (
     <>
-      <h1 class="text-3x1 font-bold underline">Nathaniel Mastracci</h1>
-
+      <div>
+        <Header />
+        <h1 class="text-3x1 font-bold">Nathaniel Mastracci</h1>
+        <h2>About me</h2>
+        <h2>Projects</h2>
+        <h2>Contact</h2>
+      </div>
     </>
   )
 }
