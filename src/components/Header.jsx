6f52a0d89b7da02f78935d422f38c1109217c28b
Mastracci <38020285+MastracciN@users.noreply.github.com>
2025-07-23 13:13:11 -0400

    header rounded and centered

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Header() {
+const Header = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => setScrolled(window.scrollY > 10);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <header className="bg-white shadow-md sticky top-0 z-50">
-            <nav className="max-w-6xl mx-auto px-4 py-4 flex justify-center items-center">
-            <div className="text-xl font-bold">NM</div>
-            <ul className="flex space-x-6 text-sm font-medium">
-                <li><a href="#intro" className="hover:text-blue-500">Home</a></li>
-                <li><a href="#about" className="hover:text-blue-500">About</a></li>
-                <li><a href="#projects" className="hover:text-blue-500">Projects</a></li>
-                <li><a href="/Resume.pdf" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">Resume</a></li>
-            </ul>
+        <header className={`sticky top-2 z-50 flex justify-center transition-all duration-300 ${
+            // scrolled ? "bg-white shadow-md" : "bg-transparent"
+           scrolled ? "bg-transparent" : "bg-transparent"
+      }`}>
+            {/* <nav className="max-w-6xl mx-auto px-4 py-4 flex justify-center items-center"> */}
+              <nav className={`inline-flex px-8 py-4 bg-white rounded-xl shadow-md items-center space-x-10`}>
+                {/* <div className="text-xl font-bold">NM</div> */}
+                <ul className="flex space-x-6 text-md font-semibold">
+                    <li><a href="#intro" className="hover:text-blue-500 py-4">Home</a></li>
+                    <li><a href="#about" className="hover:text-blue-500 py-4">About</a></li>
+                    <li><a href="#projects" className="hover:text-blue-500 py-4">Projects</a></li>
+                    <li><a href="/Resume.pdf" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500 py-4">Resume</a></li>
+                </ul>
             </nav>
         </header>
     );
