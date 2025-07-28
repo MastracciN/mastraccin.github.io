962af6c2ea8612cabdc63614a3a1df3682c7e971
Mastracci <38020285+MastracciN@users.noreply.github.com>
2025-07-27 21:32:13 -0400

    spacing changes, sections are tighter, text color changes, links section begun

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -12,37 +12,45 @@ const Portfolio = () => {
       <div className="max-w-3xl mx-auto px-4">
 
       {/* Intro Section */}
-      <section id="intro" className="flex flex-col lg:flex-row  items-center text-left space-x-8 px-6">
+      <section id="intro" className="flex flex-col lg:flex-row  items-center text-left space-x-8 px-6 py-10">
         <img src={pic} alt="Profile" className="w-52 aspect-square rounded-full object-cover mb-6 lg:mb-0"/>
         <div>
-          <h1 className="text-5xl mb-4">Nathaniel Mastracci</h1>
-          <p className="text-lg text-gray-200">I usually <s>break</s> build things.</p>
+          <h1 className="text-5xl mb-1">Nathaniel Mastracci</h1>
+          <p className="text-lg text-gray-400">I usually <s>break</s> build things.</p>
         </div>
       </section>
 
       {/* About Section */}
-      <section id="about" className="py-10 px-6">
-        <div className="">
-          <h2 className="text-3xl mb-6">About Me</h2>
-          <p className="text-lg">I enjoy coding the most when I'm building solutions for others. 
+      <section id="about" className="px-6 mb-5">
+        <h2 className="text-2xl mb-1">About</h2>
+        <p className="text-lg text-gray-400">I enjoy coding the most when I'm building solutions for others. 
             I want to work on projects that solve real problems, no matter how big or small.</p>
-        </div>
       </section>
 
       {/* TODO: projects centered, image on left, desc. on right */}
       {/* Projects Section */}
       <section id="projects" className="px-6">
-          <h2 className="text-4xl mb-10">Projects</h2>
+          <h2 className="text-2xl mb-2">Projects</h2>
           {/* Project 1 */}
           <div className="rounded-2xl">
-            <h3 className="text-xl font-semibold mb-2">Clearer</h3>
-            <p className="mb-4">An assistive reading tool for children with Auditory Processing Disorder built in Android.</p>
+            <h3 className="text-xl mb-2">Clearer</h3>
+            <p className="mb-4 text-gray-400">An assistive reading tool for children with Auditory Processing Disorder built in Android.</p>
             <a href="https://github.com/MastracciN/Clearer" className="text-blue-600 hover:underline">View Project →</a>
           </div>
       </section>
 
+      <section id="links" className="py-10 px-6">
+        <h2 className="text-4xl mb-10">Links</h2>
+        <div className="grid grid-cols-[auto_auto]">
+          <div className="">Item 1</div>
+          <div className="">Item 2</div>
+          <div className="">Item 2</div>
+          <div className="">Item 2</div>
+        </div>
+      </section>
+
       {/* Skills Section */}
-      <Skills />
+      {/* <Skills /> */}
 
     {/* Contact Section */}
       {/* <section id="contact" className="py-20 px-6 bg-white dark:bg-gray-900">
