e5a223d10c4f15d0cbe4e4cb9d380fa56598d407
Mastracci <38020285+MastracciN@users.noreply.github.com>
2025-07-28 19:11:51 -0400

    work experience section completed

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -13,37 +13,71 @@ const Portfolio = () => {
 
       {/* Intro Section */}
       <section id="intro" className="flex flex-col lg:flex-row  items-center text-left space-x-8 px-6 py-10">
-        <img src={pic} alt="Profile" className="w-52 aspect-square rounded-full object-cover mb-6 lg:mb-0"/>
+        <img src={pic} alt="Profile" className="w-45 aspect-square rounded-full object-cover mb-6 lg:mb-0"/>
         <div>
-          <h1 className="text-5xl mb-1">Nathaniel Mastracci</h1>
+          <h1 className="text-4xl mb-1">Nathaniel Mastracci</h1>
           <p className="text-lg text-gray-400">
+            Full-Stack Developer
+          </p>
+          <p className="text-lg text-gray-500">
             I usually <s>break</s> build things.
           </p>
         </div>
       </section>
 
       {/* About Section */}
-      <section id="about" className="px-6 mb-7">
-        <h2 className="text-2xl mb-3">About</h2>
+      <section id="about" className="px-6 mb-10">
+        <h2 className="text-2xl mb-5">About</h2>
         <p className="text-gray-400">I enjoy coding the most when I'm building solutions for others. 
             I want to work on projects that solve real problems, no matter how big or small.</p>
       </section>
 
       {/* Work Experience Section */}
-      <section id="workexperience" className="px-6 mb-7">
-        <h2 className="text-2xl mb-3">Work Experience</h2>
-        <p className="text-gray-400">I enjoy coding the most when I'm building solutions for others. 
-            I want to work on projects that solve real problems, no matter how big or small.</p>
+      <section id="workexperience" className="px-6 mb-10">
+        <h2 className="text-2xl mb-5">Work Experience</h2>
+        {/* Experience 1 */}
+        <div className="flex">
+          <p className="mb-1">Research Assistant — Sheridan Centre for Applied AI</p>
+          <p className="text-gray-500 mb-1 ml-auto">Jan. 2023 - Apr. 2024</p>
+        </div>
+        <p className="text-gray-400 mb-1">Developed a machine learning model for the classification of heart sounds collected via electronic stethoscope.
+          Presented and published a paper on this research available here.
+        </p>
+        <p className="text-gray-500 mb-5">Python • TensorFlow • Pandas • NumPy</p>
+
+        {/* Experience 2 */}
+        <div className="flex">
+          <p className="mb-1">Research Assistant — Sheridan Centre for Applied AI</p>
+          <p className="text-gray-500 mb-1 ml-auto">May 2022 - Aug. 2023</p>
+        </div>
+        <p className="text-gray-400 mb-1">Researched state-of-the-art voice cloning solutions, emphasizing cross-lingual capabilities, AI model effectiveness,
+           and optimal dataset practices. Developed voice clones with Python and PyCaret, utilizing Wav2Lip to produce AI-generated lip-synced videos and explore
+           practical applications of voice cloning technology.
+        </p>
+        <p className="text-gray-500 mb-5">Python • PyCaret • NumPy</p>
+
+        {/* Experience 3 */}
+        <div className="flex">
+          <p className="mb-1">Front-End Developer — Claret Asset Management</p>
+          <p className="text-gray-500 mb-1 ml-auto">July 2019 - Apr. 2020</p>
+        </div>
+        <p className="text-gray-400 mb-1">Developed the front end of a client-onboarding web application, creating extensive forms to manage client personal
+          and financial information.
+        </p>
+        <p className="text-gray-500 mb-5">AngularJS • JavaScript • HTML • CSS</p>
+
+
       </section>
 
       {/* Projects Section */}
-      <section id="projects" className="px-6 mb-7">
-          <h2 className="text-2xl mb-3">Projects</h2>
+      <section id="projects" className="px-6 mb-10">
+          <h2 className="text-2xl mb-5">Projects</h2>
           {/* Project 1 */}
           <div className="rounded-2xl">
-            <h3 className="text-xl mb-1">Clearer</h3>
+            <a href="https://github.com/MastracciN/Clearer" className="text-lg mb-1 hover:underline"
+              target="_blank" rel="noopener noreferrer">Clearer →</a>
             <p className="text-gray-400 mb-1">An assistive reading tool for children with Auditory Processing Disorder built in Android.</p>
-            <a href="https://github.com/MastracciN/Clearer" className="text-blue-600 hover:underline">View Project →</a>
+            <p className="text-gray-500 mb-1">Kotlin • React • Firestore • Figma</p>
           </div>
       </section>
 
