8136c23314c6d57995de3f5b0f20249217b31ff8
Mastracci <38020285+MastracciN@users.noreply.github.com>
2025-07-31 11:45:47 -0400

    added HoverText function to change build to break on hover

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header.jsx"
 import Skills from "./components/Skills.jsx"
+import Build from "./components/Build.jsx"
 import pic from './assets/profile.jpg'
 
 const Portfolio = () => {
@@ -12,28 +13,29 @@ const Portfolio = () => {
       <div className="relative lg:flex lg:w-5/7 mx-auto">
         {/* Intro Section */}
         <section id="intro" 
-          className="flex flex-col items-center text-left px-6 py-10
-             lg:sticky lg:top-0 lg:h-screen lg:w-2/5 lg:items-center lg:justify-start lg:pt-[10%]">
+          className="flex flex-col  text-left px-6 py-10
+             lg:sticky lg:top-0 lg:h-screen lg:w-2/5 lg:pt-[10%] lg:pl-[10%]">
           <img src={pic} alt="Profile" className="w-70 aspect-square rounded-full object-cover mb-6 lg:mb-10"/>
           <div>
             <h1 className="text-4xl mb-1">Nathaniel Mastracci</h1>
             <p className="text-lg text-gray-400">Full-Stack Developer</p>
-            <p className="text-lg text-gray-500">
+            {/* <p className="text-lg text-gray-500">
               I usually <s>break</s> build things.
-            </p>
+            </p> */}
+            <Build />
           </div>
 
           {/* Links */}
-          <div className="flex flex-row py-5 gap-x-3">
+          <div className="flex flex-row py-8 gap-x-3 justify-start">
             <a href="https://github.com/MastracciN" target="_blank" rel="noopener noreferrer"
               className="opacity-60 hover:opacity-100 transition-opacity duration-200">
-              <svg viewBox="0 0 128 128" width="35" height="35">
+              <svg viewBox="0 0 128 128" width="30" height="30">
                 <g fill="#ffffff"><path fill-rule="evenodd" clip-rule="evenodd" d="M64 5.103c-33.347 0-60.388 27.035-60.388 60.388 0 26.682 17.303 49.317 41.297 57.303 3.017.56 4.125-1.31 4.125-2.905 0-1.44-.056-6.197-.082-11.243-16.8 3.653-20.345-7.125-20.345-7.125-2.747-6.98-6.705-8.836-6.705-8.836-5.48-3.748.413-3.67.413-3.67 6.063.425 9.257 6.223 9.257 6.223 5.386 9.23 14.127 6.562 17.573 5.02.542-3.903 2.107-6.568 3.834-8.076-13.413-1.525-27.514-6.704-27.514-29.843 0-6.593 2.36-11.98 6.223-16.21-.628-1.52-2.695-7.662.584-15.98 0 0 5.07-1.623 16.61 6.19C53.7 35 58.867 34.327 64 34.304c5.13.023 10.3.694 15.127 2.033 11.526-7.813 16.59-6.19 16.59-6.19 3.287 8.317 1.22 14.46.593 15.98 3.872 4.23 6.215 9.617 6.215 16.21 0 23.194-14.127 28.3-27.574 29.796 2.167 1.874 4.097 5.55 4.097 11.183 0 8.08-.07 14.583-.07 16.572 0 1.607 1.088 3.49 4.148 2.897 23.98-7.994 41.263-30.622 41.263-57.294C124.388 32.14 97.35 5.104 64 5.104z"></path><path d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0"></path></g>
               </svg>
             </a>
             <a href="https://www.linkedin.com/in/mastracci/" target="_blank" rel="noopener noreferrer"
               className="opacity-60 hover:opacity-100 transition-opacity duration-200">
-              <svg viewBox="0 0 128 128" width="35" height="35">
+              <svg viewBox="0 0 128 128" width="30" height="30">
                 <g fill="#ffffff"><path d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3zM39.17 107H21.06V48.73h18.11zm-9-66.21a10.5 10.5 0 1110.49-10.5 10.5 10.5 0 01-10.54 10.48zM107 107H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53V48.73h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75z"></path></g>
               </svg>
             </a>
@@ -44,7 +46,7 @@ const Portfolio = () => {
         {/* Right Column */}
         <div className="w-full lg:w-3/5 overflow-y-auto lg:p-10">
           {/* About Section */}
-          <section id="about" className="px-6 mb-10">
+          <section id="about" className="px-6 mb-15">
             <h2 className="text-2xl mb-5">About</h2>
             <p className="text-gray-400">I enjoy coding the most when I'm building solutions for others. 
                 I want to work on projects that solve real problems, no matter how big or small.</p>
@@ -52,38 +54,32 @@ const Portfolio = () => {
 
           {/* Work Experience Section */}
           {/* gradients on work experiences, fortnite */}
-          <section id="workexperience" className="px-6 mb-10">
+          <section id="workexperience" className="px-6 mb-15">
             <h2 className="text-2xl mb-5">Work Experience</h2>
             {/* Experience 1 */}
-            <div className="flex">
-              <p className="mb-1">Research Assistant — Sheridan Centre for Applied AI</p>
-              <p className="text-gray-500 mb-1 ml-auto">2023 — 2024</p>
-            </div>
+            <p className="text-gray-500 mb-1 ml-auto">2023 — 2024</p>
+            <p className="mb-1">Research Assistant • Sheridan Centre for Applied AI</p>
             <p className="text-gray-400 mb-1">Developed a machine learning model for the classification of heart sounds collected via electronic stethoscope.
               Presented and published a paper on this research available here.
             </p>
-            <p className="text-gray-500 mb-5">Python • TensorFlow • Pandas • NumPy</p>
+            <p className="text-gray-500 mb-8">Python • TensorFlow • Pandas • NumPy</p>
 
             {/* Experience 2 */}
-            <div className="flex">
-              <p className="mb-1">Research Assistant — Sheridan Centre for Applied AI</p>
-              <p className="text-gray-500 mb-1 ml-auto">May — Aug 2022</p>
-            </div>
+            <p className="text-gray-500 mb-1 ml-auto">May — Aug 2022</p>
+            <p className="mb-1">Research Assistant • Sheridan Centre for Applied AI</p>
             <p className="text-gray-400 mb-1">Researched state-of-the-art voice cloning solutions, emphasizing cross-lingual capabilities, AI model effectiveness,
               and optimal dataset practices. Developed voice clones with Python and PyCaret, utilizing Wav2Lip to produce AI-generated lip-synced videos and explore
               practical applications of voice cloning technology.
             </p>
-            <p className="text-gray-500 mb-5">Python • PyCaret • NumPy</p>
+            <p className="text-gray-500 mb-8">Python • PyCaret • NumPy</p>
 
             {/* Experience 3 */}
-            <div className="flex">
-              <p className="mb-1">Front-End Developer — Claret Asset Management</p>
-              <p className="text-gray-500 mb-1 ml-auto">2019 — 2020</p>
-            </div>
+            <p className="text-gray-500 mb-1 ml-auto">2019 — 2020</p>
+            <p className="mb-1">Front-End Developer • Claret Asset Management</p>
             <p className="text-gray-400 mb-1">Developed the front end of a client-onboarding web application, creating extensive forms to manage client personal
               and financial information.
             </p>
-            <p className="text-gray-500 mb-5">AngularJS • JavaScript • HTML • CSS</p>
+            <p className="text-gray-500 mb-8">AngularJS • JavaScript • HTML • CSS</p>
 
             <div className="text-blue-600 hover:underline">
               <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">View Full Resume ↗</a>
@@ -93,7 +89,7 @@ const Portfolio = () => {
           </section>
 
           {/* Projects Section */}
-          <section id="projects" className="px-6 mb-10">
+          <section id="projects" className="px-6 mb-15">
               <h2 className="text-2xl mb-5">Projects</h2>
               {/* Project 1 */}
               <div className="rounded-2xl">
---
+++ src/components/Build.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function HoverText() {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <p className="text-lg text-gray-500">
+      Sometimes I {" "}
+      <span
+        className="font-bold text-gray-400"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        {hovered ? "break" : "build"}
+      </span>{" "}
+      things.
+    </p>
+  );
+}
+
+
+
+
